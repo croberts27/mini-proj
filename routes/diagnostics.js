6745ce013fba6a112c31d1a4dac5e0bfc320feb8
Calvin Roberts <you@example.com>
2023-06-15 09:58:53 -0600

    added GET requests

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -3,13 +3,18 @@ const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
+diagnostics.get('/', async (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  const data = await readFromFile('db/diagnostics.json', 'utf-8');
+  console.log(data)
+  res.json(data)
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  readAndAppend(req.body, 'db/diagnostics.json')
+  res.json();
 });
 
 module.exports = diagnostics;
